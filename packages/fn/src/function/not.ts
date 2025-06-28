0c22a6c497ce69df92061097933f0199695e9d89
Michael Ostermann <michaelostermann@me.com>
2025-06-28 20:22:35 +0200

    fix(function/not): add missing NoInfer

--- packages/fn/src/function/not.ts
+++ packages/fn/src/function/not.ts
@@ -23,11 +23,11 @@ import { dual } from "@monstermann/dfdl"
  * ```
  */
 export const not: {
-    <T, U extends T>(predicate: (value: T) => value is U): (target: T) => target is Exclude<T, U>
-    <T>(predicate: (value: T) => boolean): (target: T) => boolean
+    <T, U extends T>(predicate: (value: NoInfer<T>) => value is U): (target: T) => target is Exclude<T, U>
+    <T>(predicate: (value: NoInfer<T>) => boolean): (target: T) => boolean
 
-    <T, U extends T>(target: T, predicate: (value: T) => value is U): target is Exclude<T, U>
-    <T>(target: T, predicate: (value: T) => boolean): boolean
-} = dual(2, <T, U extends T>(target: T, predicate: (value: T) => T): target is Exclude<T, U> => {
+    <T, U extends T>(target: T, predicate: (value: NoInfer<T>) => value is U): target is Exclude<T, U>
+    <T>(target: T, predicate: (value: NoInfer<T>) => boolean): boolean
+} = dual(2, <T, U extends T>(target: T, predicate: (value: NoInfer<T>) => T): target is Exclude<T, U> => {
     return !predicate(target)
 })
