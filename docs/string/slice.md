a5c96bd91c8b64153f8a4dada69858c0a1ba25d2
Michael Ostermann <michaelostermann@me.com>
2025-07-06 13:52:11 +0200

    feat(string/slice): make last param optional

--- docs/string/slice.md
+++ docs/string/slice.md
@@ -1,19 +1,21 @@
 # slice
 
-`slice(target, start, end)`
+`slice(target, start, end?)`
 
-Extracts a section of `target` string from `start` index to `end` index (exclusive).
+Extracts a section of `target` string from `start` index to `end` index (exclusive). If `end` is not provided, extracts to the end of the string.
 
 ::: code-group
 
 ```ts [data-first]
 slice("hello world", 0, 5); // "hello"
 slice("hello world", 6, 11); // "world"
+slice("hello world", 6); // "world"
 ```
 
 ```ts [data-last]
 pipe("hello world", slice(0, 5)); // "hello"
 pipe("hello world", slice(6, 11)); // "world"
+pipe("hello world", slice(6)); // "world"
 ```
 
 :::
--- packages/fn/src/string/slice.ts
+++ packages/fn/src/string/slice.ts
@@ -1,29 +1,26 @@
 import type { Slice } from "string-ts"
+import { dfdlT } from "@monstermann/dfdl"
 import { slice as apply } from "string-ts"
 
 /**
- * `slice(target, start, end)`
+ * `slice(target, start, end?)`
  *
- * Extracts a section of `target` string from `start` index to `end` index (exclusive).
+ * Extracts a section of `target` string from `start` index to `end` index (exclusive). If `end` is not provided, extracts to the end of the string.
  *
  * ```ts
  * slice("hello world", 0, 5); // "hello"
  * slice("hello world", 6, 11); // "world"
+ * slice("hello world", 6); // "world"
  * ```
  *
  * ```ts
  * pipe("hello world", slice(0, 5)); // "hello"
  * pipe("hello world", slice(6, 11)); // "world"
+ * pipe("hello world", slice(6)); // "world"
  * ```
  */
-export function slice<T extends string, U extends number>(start: U): (target: T) => Slice<T, U>
-export function slice<T extends string, U extends number, V extends number>(start: U, end: V): (target: T) => Slice<T, U, V>
+export const slice: {
+    <T extends string, U extends number, V extends number | undefined = undefined>(start: U, end?: V): (target: T) => Slice<T, U, V>
 
-export function slice<T extends string, U extends number>(target: T, start: U): Slice<T, U>
-export function slice<T extends string, U extends number, V extends number>(target: T, start: U, end: V): Slice<T, U, V>
-
-export function slice(a: any, b?: any, c?: any): any {
-    return typeof a === "string"
-        ? apply(a, b, c)
-        : (d: any) => apply(d, a, b)
-}
+    <T extends string, U extends number, V extends number | undefined = undefined>(target: T, start: U, end?: V): Slice<T, U, V>
+} = dfdlT(apply, args => typeof args[0] === "string")
