8615e64d858df43fa97db11ff7e2d5007cc230a8
Michael Ostermann <michaelostermann@me.com>
2025-07-06 13:52:34 +0200

    feat(array/groupBy): support data-first data-last signatures

--- docs/array/groupBy.md
+++ docs/array/groupBy.md
@@ -4,11 +4,24 @@
 
 Groups elements in `array` by the result of calling `grouper` function on each element, optionally transforming each element with `transform`, returning an object with keys as group values and values as arrays of elements.
 
-```ts
+::: code-group
+
+```ts [data-first]
 groupBy(
     [1, 2, 3, 4],
     (x) => (x % 2 === 0 ? "even" : "odd"),
     (x) => x * 10,
-);
-// { odd: [10, 30], even: [20, 40] }
+); // { even: [20, 40], odd: [10, 30] }
 ```
+
+```ts [data-last]
+pipe(
+    [1, 2, 3, 4],
+    groupBy(
+        (x) => (x % 2 === 0 ? "even" : "odd"),
+        (x) => x * 10,
+    ),
+); // { even: [20, 40], odd: [10, 30] }
+```
+
+:::
--- packages/fn/src/array/groupBy.ts
+++ packages/fn/src/array/groupBy.ts
@@ -1,3 +1,5 @@
+import { dfdlT } from "@monstermann/dfdl"
+
 /**
  * `groupBy(array, grouper, transform?)`
  *
@@ -8,28 +10,39 @@
  *     [1, 2, 3, 4],
  *     (x) => (x % 2 === 0 ? "even" : "odd"),
  *     (x) => x * 10,
- * );
+ * ); // { even: [20, 40], odd: [10, 30] }
+ * ```
+ *
+ * ```ts
+ * pipe(
+ *     [1, 2, 3, 4],
+ *     groupBy(
+ *         (x) => (x % 2 === 0 ? "even" : "odd"),
+ *         (x) => x * 10,
+ *     ),
+ * ); // { even: [20, 40], odd: [10, 30] }
+ * ```
  */
-export function groupBy<T extends object, U extends PropertyKey, V>(
-    target: readonly T[],
-    by: (value: NoInfer<T>, idx: number, target: readonly NoInfer<T>[]) => U,
-    transform: (value: NoInfer<T>, key: U, idx: number, target: readonly NoInfer<T>[]) => V,
-): Record<U, V[]>
+export const groupBy: {
+    <T extends object, U extends PropertyKey, V>(
+        target: readonly T[],
+        by: (value: NoInfer<T>, idx: number, target: readonly NoInfer<T>[]) => U,
+        transform: (value: NoInfer<T>, key: U, idx: number, target: readonly NoInfer<T>[]) => V,
+    ): Record<U, V[]>
 
-export function groupBy<T extends object, U extends PropertyKey>(
-    target: readonly T[],
-    by: (value: NoInfer<T>, idx: number, target: readonly NoInfer<T>[]) => U,
-): Record<U, T[]>
-
-export function groupBy<T extends object, U extends PropertyKey, V>(
+    <T extends object, U extends PropertyKey>(
+        target: readonly T[],
+        by: (value: NoInfer<T>, idx: number, target: readonly NoInfer<T>[]) => U,
+    ): Record<U, T[]>
+} = dfdlT(<T extends object, U extends PropertyKey, V>(
     target: readonly T[],
     by: (value: NoInfer<T>, idx: number, target: readonly NoInfer<T>[]) => U,
     transform?: (value: NoInfer<T>, key: U, idx: number, target: readonly NoInfer<T>[]) => V,
-): Record<U, (T | V)[]> {
+): Record<U, (T | V)[]> => {
     return target.reduce((acc, value, idx) => {
         const key = by(value, idx, target)
         acc[key] ??= []
         acc[key].push(transform ? transform(value, key, idx, target) : value)
         return acc
     }, {} as Record<U, (T | V)[]>)
-}
+}, args => Array.isArray(args[0]))
